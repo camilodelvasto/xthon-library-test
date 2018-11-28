324c7827c883fc4b7804787b99bf8b5fb3bdc8aa
Camilo Delvasto <camilodelvasto@gmail.com>
2018-11-28 12:51:36 -0500

    Change component name

--- src/lib.js
+++ src/lib.js
@@ -1,9 +1,9 @@
 import Vue from 'vue';
-import AppHeader from '@/components/RideForGood/AppHeader.vue';
+import AppHeaderx from '@/components/RideForGood/AppHeader.vue';
 import AppBanner from '@/components/RideForGood/AppBanner.vue';
 
 const Components = {
-  AppHeader,
+  AppHeaderx,
   AppBanner
 }
 

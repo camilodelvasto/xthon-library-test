aa5a8d35319663ee8796b8ff1fd17a20e9c66a78
Camilo Delvasto <camilodelvasto@gmail.com>
2018-11-29 12:13:11 -0500

    x

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
     config.module.rules.delete('eslint');
     config.merge({
       externals: {
-        Router: 'vue-router',
+        "$router": 'vue-router',
         Vuex: "vuex"
       }
     })
